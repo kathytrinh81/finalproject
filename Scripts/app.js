60c8dbe916ea71e8d80018e39345fbffd39b68f1
kathytrinh81 <kathytrinh81@yahoo.com>
2015-08-12 02:00:16 -0600

    final
    here’s my final.

---
+++ Scripts/app.js
@@ -0,0 +1,86 @@
+﻿function ViewModel() {
+    var self = this;
+
+    var tokenKey = 'accessToken';
+
+    self.result = ko.observable();
+    self.user = ko.observable();
+
+    self.registerUserName = ko.observable();
+    self.registerPassword = ko.observable();
+    self.registerPassword2 = ko.observable();
+
+    self.loginUserName = ko.observable();
+    self.loginPassword = ko.observable();
+
+    function showError(jqXHR) {
+        self.result(jqXHR.status + ': ' + jqXHR.statusText);
+    }
+
+    self.callApi = function () {
+        self.result('');
+
+        var token = sessionStorage.getItem(tokenKey);
+        var headers = {};
+        if (token) {
+            headers.Authorization = 'Bearer ' + token;
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: '/api/values',
+            headers: headers
+        }).done(function (data) {
+            self.result(data);
+        }).fail(showError);
+    }
+
+    // Register FIRST using username: api-user and password: 1234pass
+    self.register = function () {
+        self.result('');
+
+        var data = {
+            UserName: self.registerUserName(),
+            Password: self.registerPassword(),
+            ConfirmPassword: self.registerPassword2()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/Account/Register',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (data) {
+            self.result("Done!");
+        }).fail(showError);
+    }
+
+    // THEN Validate by login with username: api-user and password: 1234pass
+    self.login = function () {
+        self.result('');
+
+        var loginData = {
+            grant_type: 'password',
+            username: self.loginUserName(),
+            password: self.loginPassword()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/Token',
+            data: loginData
+        }).done(function (data) {
+            self.user(data.userName);
+            // Cache the access token in session storage.
+            sessionStorage.setItem(tokenKey, data.access_token);
+        }).fail(showError);
+    }
+
+    self.logout = function () {
+        self.user('');
+        sessionStorage.removeItem(tokenKey)
+    }
+}
+
+var app = new ViewModel();
+ko.applyBindings(app);
